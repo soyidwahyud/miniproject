006ef0fb54cf95197a3f09c3a3b97330f2553ec8
soyidwahyud <soyidwahyu4@gmail.com>
2019-10-06 02:44:57 +0700

    update shareprefs

--- app/src/main/java/com/example/bloodbankinventory/shareprefs.java
+++ app/src/main/java/com/example/bloodbankinventory/shareprefs.java
@@ -1,13 +1,68 @@
-package com.example.bloodbankinventory;
+    package com.example.bloodbankinventory;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 
 import static com.example.bloodbankinventory.SessionManager.pref;
 
 public class shareprefs {
     final static String FileName = "BloodBankInventory";
     public static final String IS_LOGIN = "IsLoggedIn";
+    static final String KEY_USER_TEREGISTER ="user", KEY_PASS_TEREGISTER ="pass";
+    static final String KEY_USERNAME_SEDANG_LOGIN = "Username_logged_in";
+    static final String KEY_STATUS_SEDANG_LOGIN = "Status_logged_in";
+
+    private static SharedPreferences getSharedPreference(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static void setRegisteredUser(Context context, String username){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putString(KEY_USER_TEREGISTER, username);
+        editor.apply();
+    }
+    /** Mengembalikan nilai dari key KEY_USER_TEREGISTER berupa String */
+    public static String getRegisteredUser(Context context){
+        return getSharedPreference(context).getString(KEY_USER_TEREGISTER,"");
+    }
+
+    /** Deklarasi Edit Preferences dan mengubah data
+     *  yang memiliki key KEY_PASS_TEREGISTER dengan parameter password */
+    public static void setRegisteredPass(Context context, String password){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putString(KEY_PASS_TEREGISTER, password);
+        editor.apply();
+    }
+    /** Mengembalikan nilai dari key KEY_PASS_TEREGISTER berupa String */
+    public static String getRegisteredPass(Context context){
+        return getSharedPreference(context).getString(KEY_PASS_TEREGISTER,"");
+    }
+
+
+    public static void setLoggedInUser(Context context, String username){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putString(KEY_USERNAME_SEDANG_LOGIN, username);
+        editor.apply();
+    }
+    public static String getLoggedInUser(Context context){
+        return getSharedPreference(context).getString(KEY_USERNAME_SEDANG_LOGIN,"");
+    }
+    public static void setLoggedInStatus(Context context, boolean status){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putBoolean(KEY_STATUS_SEDANG_LOGIN,status);
+        editor.apply();
+    }
+    public static boolean getLoggedInStatus(Context context){
+        return getSharedPreference(context).getBoolean(KEY_STATUS_SEDANG_LOGIN,false);
+    }
+    public static void clearLoggedInUser (Context context){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.remove(KEY_USERNAME_SEDANG_LOGIN);
+        editor.remove(KEY_STATUS_SEDANG_LOGIN);
+        editor.apply();
+    }
 
     public static String readSharedSetting(Context ctx, String settingName, String defaultValue) {
         SharedPreferences sharedPref = ctx.getSharedPreferences(FileName, Context.MODE_PRIVATE);
