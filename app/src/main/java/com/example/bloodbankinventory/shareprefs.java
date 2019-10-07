bad39e432a146e8d97f33ae209df2890deea22be
soyidwahyud <soyidwahyu4@gmail.com>
2019-10-07 15:19:39 +0700

    update sharepref

--- app/src/main/java/com/example/bloodbankinventory/shareprefs.java
+++ app/src/main/java/com/example/bloodbankinventory/shareprefs.java
@@ -14,59 +14,20 @@ public class shareprefs {
     static final String KEY_USERNAME_SEDANG_LOGIN = "Username_logged_in";
     static final String KEY_STATUS_SEDANG_LOGIN = "Status_logged_in";
 
-    private static SharedPreferences getSharedPreference(Context context){
-        return PreferenceManager.getDefaultSharedPreferences(context);
+    Context context;
+    public shareprefs(Context context) {
+        this.context = context;
     }
-
-    public static void setRegisteredUser(Context context, String username){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.putString(KEY_USER_TEREGISTER, username);
-        editor.apply();
-    }
-    /** Mengembalikan nilai dari key KEY_USER_TEREGISTER berupa String */
-    public static String getRegisteredUser(Context context){
-        return getSharedPreference(context).getString(KEY_USER_TEREGISTER,"");
-    }
-
-    /** Deklarasi Edit Preferences dan mengubah data
-     *  yang memiliki key KEY_PASS_TEREGISTER dengan parameter password */
-    public static void setRegisteredPass(Context context, String password){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.putString(KEY_PASS_TEREGISTER, password);
-        editor.apply();
-    }
-    /** Mengembalikan nilai dari key KEY_PASS_TEREGISTER berupa String */
-    public static String getRegisteredPass(Context context){
-        return getSharedPreference(context).getString(KEY_PASS_TEREGISTER,"");
-    }
-
-
-    public static void setLoggedInUser(Context context, String username){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.putString(KEY_USERNAME_SEDANG_LOGIN, username);
-        editor.apply();
-    }
-    public static String getLoggedInUser(Context context){
-        return getSharedPreference(context).getString(KEY_USERNAME_SEDANG_LOGIN,"");
-    }
-    public static void setLoggedInStatus(Context context, boolean status){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.putBoolean(KEY_STATUS_SEDANG_LOGIN,status);
-        editor.apply();
-    }
-    public static boolean getLoggedInStatus(Context context){
-        return getSharedPreference(context).getBoolean(KEY_STATUS_SEDANG_LOGIN,false);
-    }
-    public static void clearLoggedInUser (Context context){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.remove(KEY_USERNAME_SEDANG_LOGIN);
-        editor.remove(KEY_STATUS_SEDANG_LOGIN);
-        editor.apply();
-    }
-
     public static String readSharedSetting(Context ctx, String settingName, String defaultValue) {
         SharedPreferences sharedPref = ctx.getSharedPreferences(FileName, Context.MODE_PRIVATE);
         return sharedPref.getString(settingName, defaultValue);
+    }
+    public void saveLoginDetails(String username, String password) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", username);
+        editor.putString("password", password);
+        editor.commit();
     }
 
     public static void saveSharedSetting(Context ctx, String settingName, String settingValue) {
@@ -75,7 +36,39 @@ public class shareprefs {
         editor.putString(settingName, settingValue);
         editor.apply();
     }
+    public boolean isUserLogedOut() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        boolean isUsernameEmpty = sharedPreferences.getString("username", "").isEmpty();
+        boolean isPasswordEmpty = sharedPreferences.getString("password", "").isEmpty();
+        return isUsernameEmpty || isPasswordEmpty;
+    }
+
+    public boolean Logout(){
+        SharedPreferences sp = context.getSharedPreferences("LoginDetails",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sp.edit();
+        editor.remove("username");
+        editor.remove("password");
+        editor.commit();
+        return true;
+    }
+    public static void clearLoggedInUser (Context context){
+        SharedPreferences sharedpref = context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpref.edit();
+        editor.remove(KEY_USERNAME_SEDANG_LOGIN);
+        editor.remove(KEY_STATUS_SEDANG_LOGIN);
+        editor.apply();
+    }
     public static boolean isLoggedIn(){
         return pref.getBoolean(IS_LOGIN, false);
+    }
+
+    private static SharedPreferences getSharedPreference(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+    public static void LoggedInUser (Context context){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.remove(KEY_USERNAME_SEDANG_LOGIN);
+        editor.remove(KEY_STATUS_SEDANG_LOGIN);
+        editor.apply();
     }
 }
